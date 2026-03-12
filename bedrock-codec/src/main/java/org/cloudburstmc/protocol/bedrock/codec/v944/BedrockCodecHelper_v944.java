7c86f01a6aa988781d257fbb7e4fdb3c53808fc6
Kaooot <37877491+Kaooot@users.noreply.github.com>
2026-03-12 12:28:16 +0100

    Fix BlockPosition serialization

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v944/BedrockCodecHelper_v944.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v944/BedrockCodecHelper_v944.java
@@ -1,6 +1,7 @@
 package org.cloudburstmc.protocol.bedrock.codec.v944;
 
 import io.netty.buffer.ByteBuf;
+import org.cloudburstmc.math.vector.Vector3i;
 import org.cloudburstmc.protocol.bedrock.codec.ActorDataTypeMap;
 import org.cloudburstmc.protocol.bedrock.codec.v924.BedrockCodecHelper_v924;
 import org.cloudburstmc.protocol.bedrock.data.AbilitiesIndex;
@@ -9,6 +10,7 @@ import org.cloudburstmc.protocol.bedrock.data.inventory.itemstack.request.TextPr
 import org.cloudburstmc.protocol.bedrock.data.inventory.itemstack.request.action.ItemStackRequestActionType;
 import org.cloudburstmc.protocol.bedrock.packet.InventoryTransactionPacket;
 import org.cloudburstmc.protocol.common.util.TypeMap;
+import org.cloudburstmc.protocol.common.util.VarInts;
 
 /**
  * @author Kaooot
@@ -17,6 +19,21 @@ public class BedrockCodecHelper_v944 extends BedrockCodecHelper_v924 {
 
     public BedrockCodecHelper_v944(ActorDataTypeMap entityData, TypeMap<Class<?>> gameRulesTypes, TypeMap<ItemStackRequestActionType> stackRequestActionTypes, TypeMap<ContainerEnumName> containerSlotTypes, TypeMap<AbilitiesIndex> abilities, TypeMap<TextProcessingEventOrigin> textProcessingEventOrigins) {
         super(entityData, gameRulesTypes, stackRequestActionTypes, containerSlotTypes, abilities, textProcessingEventOrigins);
+    }
+
+    @Override
+    public void writeBlockPosition(ByteBuf buffer, Vector3i blockPosition) {
+        VarInts.writeInt(buffer, blockPosition.getX());
+        VarInts.writeInt(buffer, blockPosition.getY());
+        VarInts.writeInt(buffer, blockPosition.getZ());
+    }
+
+    @Override
+    public Vector3i readBlockPosition(ByteBuf buffer) {
+        int x = VarInts.readInt(buffer);
+        int y = VarInts.readInt(buffer);
+        int z = VarInts.readInt(buffer);
+        return Vector3i.from(x, y, z);
     }
 
     @Override
