dcba0fae646968d5b9796990b58830ce5cae1c83
Kaooot <37877491+Kaooot@users.noreply.github.com>
2026-04-05 21:42:06 +0200

    Fix compile error

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v972/serializer/LocatorBarSerializer_v972.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v972/serializer/LocatorBarSerializer_v972.java
@@ -5,7 +5,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
 import org.cloudburstmc.protocol.bedrock.codec.v944.serializer.LocatorBarSerializer_v944;
-import org.cloudburstmc.protocol.bedrock.data.ServerWaypointPayload;
+import org.cloudburstmc.protocol.bedrock.data.payload.locatorbar.ServerWaypointPayload;
 import org.cloudburstmc.protocol.common.util.OptionalBoolean;
 import org.cloudburstmc.protocol.common.util.VarInts;
 
@@ -18,7 +18,7 @@ public class LocatorBarSerializer_v972 extends LocatorBarSerializer_v944 {
 
     @Override
     protected void writeServerWaypointPayload(ByteBuf buffer, BedrockCodecHelper helper, ServerWaypointPayload payload) {
-        buffer.writeIntLE(payload.getUpdateFlag());
+        this.writeUpdateFlags(buffer, helper, payload.getUpdateFlags());
         helper.writeOptional(buffer, OptionalBoolean::isPresent, payload.getIsVisible(),
                 (buf, aHelper, isVisible) -> buf.writeBoolean(isVisible.getAsBoolean()));
         helper.writeOptionalNull(buffer, payload.getWorldPosition(), this::writeWorldPosition);
@@ -33,7 +33,7 @@ public class LocatorBarSerializer_v972 extends LocatorBarSerializer_v944 {
     @Override
     protected ServerWaypointPayload readServerWaypointPayload(ByteBuf buffer, BedrockCodecHelper helper) {
         final ServerWaypointPayload payload = new ServerWaypointPayload();
-        payload.setUpdateFlag(buffer.readIntLE());
+        this.readUpdateFlags(buffer, helper, payload.getUpdateFlags());
         payload.setIsVisible(helper.readOptional(buffer, OptionalBoolean.empty(), buf -> OptionalBoolean.of(buf.readBoolean())));
         payload.setWorldPosition(helper.readOptional(buffer, null, this::readWorldPosition));
         payload.setTexturePath(helper.readOptional(buffer, null, helper::readString));
