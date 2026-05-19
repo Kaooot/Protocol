92d486b86c25ef65df2d0f5e3db590ff55607052
Kaooot <37877491+Kaooot@users.noreply.github.com>
2026-05-19 20:38:26 +0200

    Update PackInstanceId

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/payload/pack/PackInstanceId.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/payload/pack/PackInstanceId.java
@@ -1,11 +1,15 @@
 package org.cloudburstmc.protocol.bedrock.data.payload.pack;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Kaooot
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PackInstanceId {
 
     private String packID;
