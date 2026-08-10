7ca9e371bf59f3fa37e4b0a1ed97efea67fe8cb3
Kaooot <37877491+Kaooot@users.noreply.github.com>
2026-08-10 15:43:50 +0200

    Remove AddActorSerializer_v2168

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v2168/Bedrock_v2168.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v2168/Bedrock_v2168.java
@@ -114,7 +114,6 @@ public class Bedrock_v2168 extends Bedrock_v1001 {
             .protocolVersion(2168)
             .minecraftVersion("1.26.40")
             .helper(() -> new BedrockCodecHelper_v2168(ACTOR_DATA, GAME_RULE_TYPES, ITEM_STACK_REQUEST_TYPES, CONTAINER_SLOT_TYPES, PLAYER_ABILITIES, TEXT_PROCESSING_ORIGINS, ANIMATED_TEXTURE_TYPES))
-            .updateSerializer(AddActorPacket.class, AddActorSerializer_v2168.INSTANCE)
             .updateSerializer(AddItemActorPacket.class, AddItemActorSerializer_v2168.INSTANCE)
             .updateSerializer(AddPlayerPacket.class, AddPlayerSerializer_v2168.INSTANCE)
             .updateSerializer(AnvilDamagePacket.class, AnvilDamageSerializer_v2168.INSTANCE)
